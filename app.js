ade97420007d6072928acfa51b5867a940a6ff58
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-09-14 20:01:58 +0530

    Configured morgan

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+// import packages
 const express = require("express");
+const morgan = require("morgan");
 require("dotenv").config();
 
+// initialize app variables
 const app = express();
-
 const api = process.env.API_URL;
 
-app.get(api + "/", (req, res) => {
-  res.send("Hello API!");
+// middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+
+// routes
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "some_url",
+  };
+  res.send(product);
 });
 
+// listening app
 app.listen(8000, () => {
-  console.log(api);
   console.log("Server is successfully running on http://localhost:8000");
 });
