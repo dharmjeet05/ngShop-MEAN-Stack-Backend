0c8eaf6126a6a9f143c90373428f2d836b69012a
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-09-14 19:46:09 +0530

    Configured env

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
+const api = process.env.API_URL;
+
+app.get(api + "/", (req, res) => {
   res.send("Hello API!");
 });
 
 app.listen(8000, () => {
+  console.log(api);
   console.log("Server is successfully running on http://localhost:8000");
 });
